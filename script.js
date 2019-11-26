4547b6b713dc1f0aa4fbe7e70d31f390c42f956b
Benjamin <39armyguy@gmail.com>
2019-11-26 15:50:59 -0500

    fixed error

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let players = ["X", "O"]
 document.body.querySelector('.large').addEventListener("click", event => {
   if (event.target.textContent == " "){
   event.target.textContent = players[turn]
-}
 if (turn == 0){
   turn=1
 }
 else turn=0
+}
 });
